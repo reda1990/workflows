2cb414528b0bed2aee390c35368ed0314a097605
redaadari <redaadari@gmail.com>
2017-01-26 22:54:51 +0000

    Created default task

--- gulpfile.js
+++ gulpfile.js
@@ -32,4 +32,6 @@ gulp.task("compass", function(){
 	     }))
 	     .on("error", gutil.log)
 	     .pipe(gulp.dest("builds/development/css"))
-})
+});
+
+gulp.task("default", ["coffee","js","compass"]);
