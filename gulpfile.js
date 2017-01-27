33fd00febf2cc9cc6bbae5c9a1d2adae8d5600f4
redaadari <redaadari@gmail.com>
2017-01-27 22:25:29 +0000

    Added Environment Variables

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,22 @@ var gulp = require("gulp"),
     connect = require("gulp-connect"),
     concat = require("gulp-concat");
 
-var coffeeSources = ["components/coffee/tagline.coffee"];
-var jsSources = ["components/scripts/*.js"];
-var sassSources = ["components/sass/style.scss"];
-var htmlSources = ["builds/development/*.html"];
-var jsonSources = ["builds/development/js/*.json"];
+var env,sassStyle,coffeeSources,jsSources,sassSources,htmlSources,jsonSources,outputDir;
+
+env = process.env.NODE_ENV || "development";
+if(env === "development"){
+   outputDir = "builds/development/";
+   sassStyle = "expanded";
+}
+else{
+  outputDir = "builds/production/";
+  sassStyle = "compressed";
+}
+coffeeSources = ["components/coffee/tagline.coffee"];
+jsSources = ["components/scripts/*.js"];
+sassSources = ["components/sass/style.scss"];
+htmlSources = [outputDir+"*.html"];
+jsonSources = [outputDir+"js/*.json"];
 gulp.task("coffee", function(){
     gulp.src(coffeeSources)
        .pipe(coffee({ bare: true })
@@ -23,7 +34,7 @@ gulp.task("js", function(){
     gulp.src(jsSources)
         .pipe(concat("script.js"))
         .pipe(browserify())
-        .pipe(gulp.dest("builds/development/js"))
+        .pipe(gulp.dest(outputDir+"js"))
         .pipe(connect.reload());
 });
 
@@ -31,11 +42,11 @@ gulp.task("compass", function(){
 	gulp.src(sassSources)
 	     .pipe(compass({
 	     	sass: "components/sass",
-	     	image: "builds/development/images",
-	     	style: "expanded"
+	     	image: outputDir+"images",
+	     	style: sassStyle
 	     }))
 	     .on("error", gutil.log)
-	     .pipe(gulp.dest("builds/development/css"))
+	     .pipe(gulp.dest(outputDir+"css"))
        .pipe(connect.reload());
 });
 
@@ -49,7 +60,7 @@ gulp.task("watch", function(){
 
 gulp.task("connect", function(){
    connect.server({
-      root: "builds/development/",
+      root: outputDir,
       livereload: true
    });
 });
